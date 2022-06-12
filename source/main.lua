1c38bb01237cece3ba5ba2a602aa971c9a57d0b4
HPTR <76168685+HPTR@users.noreply.github.com>
2022-06-12 16:34:35 +0100

    Add coin sprite and random placement functionality

--- source/main.lua
+++ source/main.lua
@@ -8,18 +8,32 @@ local gfx <const> = playdate.graphics
 local playerSpeed = 4
 local playTimer = nil
 local playTime = 30 * 1000
+local coinSprite = nil
 
 -- Timer function
 local function resetTimer()
 	playTimer = playdate.timer.new(playTime, playTime, 0, playdate.easingFunctions.linear)
 end
 
+-- Chooses a random location to place the coin
+local function moveCoin()
+	local randX = math.random(40, 360)
+	local randY = math.random(40, 200)
+	coinSprite:moveTo(randX, randY)
+end
+
 local function initialise()
+	math.randomseed(playdate.geSecondsSinceEpoch())
 	-- Adds player sprite to middle of screen
 	local playerImage = gfx.image.new("images/player")
 	playerSprite = gfx.sprite.new(playerImage)
 	playerSprite:moveTo(200, 120)
 	playerSprite:add()
+
+	local coinImage = gfx.image.new("images/coin")
+	coinSprite = gfx.sprite.new(coinImage)
+	moveCoin()
+	coinSprite:add()
 
 	-- Adds background to edge of screen
 	local backgroundImage = gfx.image.new("images/background")
@@ -44,21 +58,21 @@ function playdate.update()
 			moveCoin()
 		end
 	else
-	-- These 4 if statements handle movement, change speed by changing playerSpeed variable
-	if playdate.buttonIsPressed(playdate.kButtonUp) then
-		playerSprite:moveBy(0, -playerSpeed)
-	end
+		-- These 4 if statements handle movement, change speed by changing playerSpeed variable
+		if playdate.buttonIsPressed(playdate.kButtonUp) then
+			playerSprite:moveBy(0, -playerSpeed)
+		end
 
-	if playdate.buttonIsPressed(playdate.kButtonRight) then
-		playerSprite:moveBy(playerSpeed, 0)
-	end
+		if playdate.buttonIsPressed(playdate.kButtonRight) then
+			playerSprite:moveBy(playerSpeed, 0)
+		end
 
-	if playdate.buttonIsPressed(playdate.kButtonDown) then
-		playerSprite:moveBy(0, playerSpeed)
-	end
+		if playdate.buttonIsPressed(playdate.kButtonDown) then
+			playerSprite:moveBy(0, playerSpeed)
+		end
 
-	if playdate.buttonIsPressed(playdate.kButtonLeft) then
-		playerSprite:moveBy(-playerSpeed, 0)
+		if playdate.buttonIsPressed(playdate.kButtonLeft) then
+			playerSprite:moveBy(-playerSpeed, 0)
 		end
 	end
 

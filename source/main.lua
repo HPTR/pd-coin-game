ec6d90615d69e00afd6fe190a1a21864c5449902
HPTR <76168685+HPTR@users.noreply.github.com>
2022-06-12 16:22:40 +0100

    Add player sprite and background to screen

--- source/main.lua
+++ source/main.lua
@@ -2,3 +2,25 @@ import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
+local playerSprite = nil
+local gfx <const> = playdate.graphics
+
+local function initialise()
+	-- Adds player sprite to middle of screen
+	local playerImage = gfx.image.new("images/player")
+	playerSprite = gfx.sprite.new(playerImage)
+	playerSprite:moveTo(200, 120)
+	playerSprite:add()
+
+	-- Adds background to edge of screen
+	local backgroundImage = gfx.image.new("images/background")
+	gfx.sprite.setBackgroundDrawingCallback(
+		function (x, y, width, height)
+			gfx.setClipRect(x, y, width, height)
+			backgroundImage:draw(0, 0)
+			gfx.clearClipRect()
+		end
+	)
+end
+
+initialise()
